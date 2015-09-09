f26a90fbdce57d3f2c08aa1d2855888b0eb58339
Steve Engledow <steve@offend.me.uk>
2015-09-09 14:55:44 +0100

    Improve performance of dot formatter

--- formatters/dot.go
+++ formatters/dot.go
@@ -3,128 +3,76 @@ package formatters
 import (
 	"bytes"
 	"fmt"
-	"reflect"
+	"regexp"
 	"strings"
 
 	"github.com/stilvoid/please/common"
 )
 
-type node struct {
-	name  string
-	label string
-}
+var escapeRe *regexp.Regexp
+var newlineRe *regexp.Regexp
 
-type link struct {
-	left  string
-	right string
+func init() {
+	escapeRe = regexp.MustCompile(`(\\|\")`)
+	newlineRe = regexp.MustCompile(`\n`)
 }
 
 func wrap(in string) string {
-	out := strings.Replace(in, "\\", "\\\\", -1)
-	out = strings.Replace(out, "\"", "\\\"", -1)
-	out = strings.Replace(out, "\n", "\\n", -1)
-	out = fmt.Sprintf("\"%s\"", out)
+	out := strings.Replace(in, `\`, `\\`, -1)
+	out = strings.Replace(out, `"`, `\"`, -1)
+	out = strings.Replace(out, "\n", `\n`, -1)
 
-	return out
+	return "\"" + out + "\""
 }
 
-func flatten(in interface{}, currentPath string) ([]node, []link) {
-	var nodes []node
-	var links []link
+func dotNode(name string, label string) string {
+	return wrap(name) + " [label=" + wrap(label) + "];\n"
+}
 
+func dotLink(left string, right string) string {
+	return wrap(left) + " -- " + wrap(right) + ";\n"
+}
+
+func flatten(in interface{}, currentPath string, buf *bytes.Buffer) {
 	if in == nil {
-		return nodes, links
+		return
 	}
 
-	val := reflect.ValueOf(in)
-
-	switch val.Kind() {
-	case reflect.Map:
-		vv := in.(map[string]interface{})
-
+	switch vv := in.(type) {
+	case map[string]interface{}:
 		for key, value := range vv {
 			target := currentPath + "-" + key
 
-			nodes = append(nodes, node{
-				name:  target,
-				label: key,
-			})
+			buf.WriteString(dotNode(target, key))
 
 			if currentPath != "" {
-				links = append(links, link{
-					left:  currentPath,
-					right: target,
-				})
+				buf.WriteString(dotLink(currentPath, target))
 			}
 
-			newNodes, newLinks := flatten(value, target)
-
-			nodes = append(nodes, newNodes...)
-			links = append(links, newLinks...)
+			flatten(value, target, buf)
 		}
-
-		return nodes, links
-	case reflect.Array, reflect.Slice:
-		for index := 0; index < val.Len(); index++ {
-			value := val.Index(index).Interface()
-
-			target := currentPath + "-" + fmt.Sprint(index)
-
-			nodes = append(nodes, node{
-				name:  target,
-				label: fmt.Sprintf("[%d]", index),
-			})
-
-			if currentPath != "" {
-				links = append(links, link{
-					left:  currentPath,
-					right: target,
-				})
-			}
-
-			newNodes, newLinks := flatten(value, target)
-
-			nodes = append(nodes, newNodes...)
-			links = append(links, newLinks...)
-		}
-
-		return nodes, links
 	default:
 		target := currentPath + "=content"
 
-		nodes = append(nodes, node{
-			name:  target,
-			label: fmt.Sprint(in),
-		})
+		buf.WriteString(dotNode(target, fmt.Sprint(in)))
 
-		links = append(links, link{
-			left:  currentPath,
-			right: target,
-		})
-
-		return nodes, links
+		buf.WriteString(dotLink(currentPath, target))
 	}
 }
 
 func formatDot(in interface{}) (string, error) {
+	in = common.ArraysToMaps(in)
 	in = common.ForceStringKeys(in)
-
-	nodes, links := flatten(in, "root")
-
-	nodes = append(nodes, node{
-		name:  "root",
-		label: "[Root]",
-	})
 
 	var buf bytes.Buffer
 
-	for _, node := range nodes {
-		buf.WriteString(fmt.Sprintf("%s [label=%s];\n", wrap(node.name), wrap(node.label)))
-	}
+	buf.WriteString("graph{\n")
 
-	for _, link := range links {
-		buf.WriteString(fmt.Sprintf("%s -- %s;\n", wrap(link.left), wrap(link.right)))
-	}
+	buf.WriteString(dotNode("root", "[Root]"))
 
-	return fmt.Sprintf("graph{\n%s}", buf.String()), nil
+	flatten(in, "root", &buf)
+
+	buf.WriteByte('}')
+
+	return buf.String(), nil
 }
