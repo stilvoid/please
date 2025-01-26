a4e28ec53e73ea3efeb6f3882ba8f149e7514c75
Steve Engledow <steve@offend.me.uk>
2025-01-26 18:28:01 +0000

    Move path to positional arg in serve

--- cmd/serve/serve.go
+++ cmd/serve/serve.go
@@ -8,30 +8,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var path string
 var address string
 var port int
 
 func init() {
-	Cmd.Flags().StringVarP(&path, "dir", "d", "./", "Directory to serve")
 	Cmd.Flags().StringVarP(&address, "address", "a", "", "Address to listen on")
 	Cmd.Flags().IntVarP(&port, "port", "p", 8000, "Post to listen on")
 }
 
-func serveHelp() {
-	fmt.Println("Usage: please serve <PATH> [<ADDRESS>[:<PORT>]]")
-	fmt.Println()
-	fmt.Println("Serves the contents of PATH on the specified address and port.")
-	fmt.Println("Requested paths will be printed to stdout.")
-}
-
 var Cmd = &cobra.Command{
-	Use:   "serve",
-	Short: "Serve the contents of a directory through a simple web server",
+	Use:   "serve (PATH)",
+	Short: "Serve the contents of PATH (current directory if omitted) through a simple web server",
+	Args:  cobra.RangeArgs(0, 1),
 	Run: func(cmd *cobra.Command, args []string) {
+		path := "./"
+		if len(args) == 1 {
+			path = args[0]
+		}
+
 		address = fmt.Sprintf("%s:%d", address, port)
 
-		fmt.Println("Listening on", address)
+		fmt.Printf("Serving %s on %s\n", path, address)
 
 		fsys := loggingFileSystem{http.Dir(path)}
 
